897339787245afff59d38c31466774c0e9f07cdf
levier9 <rezza.d45@gmail.com>
2017-03-26 14:23:18 +0700

    First Commit

---
+++ app/src/main/java/com/example/smktelkom/appatm/pilihanbank.java
@@ -0,0 +1,37 @@
+package com.example.smktelkom.appatm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class pilihanbank extends AppCompatActivity {
+    Button btnceksaldo, btntarik, btnsetor,btntransfer, btnexit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pilihanbank);
+        btnceksaldo = (Button)findViewById(R.id.btnceksaldo);
+        btnsetor = (Button)findViewById(R.id.btnsetor);
+        btntarik = (Button)findViewById(R.id.btntarik);
+        btntransfer = (Button)findViewById(R.id.btntransfer);
+        btnexit = (Button)findViewById(R.id.btnexit);
+
+        btntarik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent (pilihanbank.this, Penarikan.class);
+                startActivity(i);
+            }
+        });
+        btnsetor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent (pilihanbank.this, Penyetoran.class);
+                startActivity(i);
+            }
+        });
+    }
+}
